2c736beba19940733c79cfbbfaa947e2666053eb
hitchhiker88 <kurtprovost88@gmail.com>
2026-05-23 06:38:47 +1000

    Create config.js

---
+++ netlify/functions/config.js
@@ -0,0 +1,13 @@
+exports.handler = async function(event) {
+  if (event.httpMethod !== 'GET') {
+    return { statusCode: 405, body: 'Method Not Allowed' };
+  }
+  return {
+    statusCode: 200,
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      ablyKey: process.env.ABLY_KEY || '',
+      elevenKey: process.env.ELEVEN_KEY || ''
+    })
+  };
+};
