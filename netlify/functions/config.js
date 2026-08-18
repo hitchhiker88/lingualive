729288e31b413c0d4047349618376b7a7aca732f
hitchhiker88 <kurtprovost88@gmail.com>
2026-08-18 16:11:07 +1000

    Update config.js

--- netlify/functions/config.js
+++ netlify/functions/config.js
@@ -1,13 +1,12 @@
 exports.handler = async function(event) {
-  if (event.httpMethod !== 'GET') {
-    return { statusCode: 405, body: 'Method Not Allowed' };
-  }
+  if (event.httpMethod !== 'GET') return { statusCode: 405, body: 'Method Not Allowed' };
   return {
     statusCode: 200,
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({
       ablyKey: process.env.ABLY_KEY || '',
-      elevenKey: process.env.ELEVEN_KEY || ''
+      elevenKey: process.env.ELEVEN_KEY || '',
+      hostPassword: process.env.HOST_PASSWORD || ''
     })
   };
 };
